d769e8e68978bf260923bba9333ae570b824557a
Alan Yang <ychengha@amazon.com>
2022-06-16 10:57:11 -0500

    update dependency files

--- QA/exp_scripts/run_main_exp.sh
+++ QA/exp_scripts/run_main_exp.sh
@@ -1,17 +1,16 @@
-export dataseed=$1
 RUN_SQUAD_EXP () {
     DATASET="squad"
     TRAIN_SIZE=${1:-500}
-    echo "${DATASET}-${TRAIN_SIZE}-dataseed_${dataseed}"
+    echo "${DATASET}-${TRAIN_SIZE}"
     echo "METHOD, ACC, AUC, F1@25, F1@50, F1@75"
-    python calib_exp/run_exp.py --data_seed ${dataseed} --method shap --train_size ${TRAIN_SIZE} --dataset ${DATASET} --do_maxprob
-    CONF_RES=$(python calib_exp/run_exp.py --data_seed ${dataseed} --method shap --train_size ${TRAIN_SIZE} --dataset ${DATASET} --do_maxprob 2>/dev/null | tail -1)
+    python calib_exp/run_exp.py --method shap --train_size ${TRAIN_SIZE} --dataset ${DATASET} --do_maxprob
+    CONF_RES=$(python calib_exp/run_exp.py --method shap --train_size ${TRAIN_SIZE} --dataset ${DATASET} --do_maxprob 2>/dev/null | tail -1)
     echo 'Conf,'${CONF_RES}
-    KAM_RES=$(python calib_exp/run_exp.py --data_seed ${dataseed} --method shap --train_size ${TRAIN_SIZE} --dataset ${DATASET} --do_baseline --arg_n_tree 300 --arg_max_depth 6  2>/dev/null  | tail -1)
+    KAM_RES=$(python calib_exp/run_exp.py --method shap --train_size ${TRAIN_SIZE} --dataset ${DATASET} --do_baseline --arg_n_tree 300 --arg_max_depth 6  2>/dev/null  | tail -1)
     echo 'KAMATH,'${KAM_RES}
-    BOW_RES=$(python calib_exp/run_exp.py --data_seed ${dataseed} --method shap --train_size ${TRAIN_SIZE} --dataset ${DATASET} --do_bow --arg_n_tree 200 --arg_max_depth 20 2>/dev/null  | tail -1)
+    BOW_RES=$(python calib_exp/run_exp.py --method shap --train_size ${TRAIN_SIZE} --dataset ${DATASET} --do_bow --arg_n_tree 200 --arg_max_depth 20 2>/dev/null  | tail -1)
     echo 'BOW,'${BOW_RES}
-    EXPL_RES=$(python calib_exp/run_exp.py --data_seed ${dataseed} --method shap --train_size ${TRAIN_SIZE} --dataset ${DATASET} --arg_n_tree 300 --arg_max_depth 20 2>/dev/null | tail -1)
+    EXPL_RES=$(python calib_exp/run_exp.py --method shap --train_size ${TRAIN_SIZE} --dataset ${DATASET} --arg_n_tree 300 --arg_max_depth 20 2>/dev/null | tail -1)
     echo 'ShapCal,'${EXPL_RES}
 }
 
